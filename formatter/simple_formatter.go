9a9688cf383d76015ecf12bc671cb5abca6a3920
Thomas Auffredou <thomas.auffredou@gmail.com>
2019-05-01 10:52:08 +0200

    feat: add "get next-version" action (#3)

---
+++ formatter/simple_formatter.go
@@ -0,0 +1,20 @@
+package formatter
+
+import "fmt"
+
+type SimpleFormatter struct {
+	Key   string
+	Value interface{}
+}
+
+func (f *SimpleFormatter) Json() {
+	fmt.Printf("{ \"%s\" : \"%s\"}\n", f.Key, f.Value)
+}
+
+func (f *SimpleFormatter) Yaml() {
+	fmt.Printf("%s: \"%s\"\n", f.Key, f.Value)
+}
+
+func (f *SimpleFormatter) Console() {
+	fmt.Println(f.Value)
+}
