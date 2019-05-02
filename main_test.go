0c021144d0c1a44d73b4ce5b2e0c7affaeb2ba1b
Thomas Auffredou <thomas.auffredou@gmail.com>
2019-05-02 20:16:34 +0200

    feat: read token from hub (#5)

--- main_test.go
+++ main_test.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestSemVer(t *testing.T) {
+func TestReadHubToken(t *testing.T) {
+	r, err := readHubToken("fixtures/config_hub.yaml")
+	assert.NoError(t, err)
+	assert.Equal(t, "xxxxxx", r)
+}
 
+func TestReadHubToken_FileNotExits(t *testing.T) {
+	_, err := readHubToken("dummy.yaml")
+	assert.Error(t, err, "")
 }
