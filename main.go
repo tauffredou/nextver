a3896ca1295fe2c1d5a1800a9ebb233ace56de6a
Thomas Auffredou <thomas.auffredou@gmail.com>
2019-07-04 17:34:42 +0200

    feat: use factory to create provider

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	log "github.com/sirupsen/logrus"
 	"github.com/tauffredou/nextver/formatter"
-	"github.com/tauffredou/nextver/model"
 	"github.com/tauffredou/nextver/provider"
 	"gopkg.in/alecthomas/kingpin.v2"
 	"os"
@@ -11,23 +10,22 @@ import (
 )
 
 var (
-	token string
+	tokenFlag = kingpin.Flag("github-token", "Github token. Can be read form hub config file").Envar("GITHUB_TOKEN").String()
 
-	repo         = kingpin.Flag("repo", "Repository").Short('r').String()
-	pattern      = kingpin.Flag("pattern", "Versionning pattern. Read from .nextver/config.yml by default").Short('p').String()
-	output       = kingpin.Flag("output", "Output format (console, json, yaml)").Short('o').Default("console").String()
-	branch       = kingpin.Flag("branch", "Target branch (default branch if empty)").Short('b').String()
-	logLevel     = kingpin.Flag("log-level", "Log level").Default("info").String()
-	providerType = kingpin.Flag("provider", "provider").Default("local").String()
+	repo     = kingpin.Flag("repo", "Repository").Default(".").Short('r').String()
+	pattern  = kingpin.Flag("pattern", "Versionning pattern. Read from .nextver/config.yml by default").Short('p').String()
+	output   = kingpin.Flag("output", "Output format (console, json, yaml)").Short('o').Default("console").String()
+	branch   = kingpin.Flag("branch", "Target branch (default branch if empty)").Short('b').String()
+	logLevel = kingpin.Flag("log-level", "Log level").Default("info").String()
 
 	color = kingpin.Flag("color", "Colorize output").Default("true").Bool()
 
 	//get
 	getCommand       = kingpin.Command("get", "")
-	_                = githubCommand(getCommand, "releases", "List releases")
-	changelogCommand = githubCommand(getCommand, "changelog", "Get changelog")
-	release          = changelogCommand.Flag("release", "Changelog for a specific release").String()
-	_                = githubCommand(getCommand, "next-version", "Get next version")
+	_                = getCommand.Command("releases", "List releases")
+	changelogCommand = getCommand.Command("changelog", "Get changelog")
+	release          = changelogCommand.Flag("release", "Changelog for a specific release").Default("").String()
+	_                = getCommand.Command("next-version", "Get next version")
 
 	//create
 	createCommand = kingpin.Command("create", "")
@@ -36,7 +34,6 @@ var (
 	_ = createCommand.Flag("template", "Template file").String()
 
 	defaultHubConfig = path.Join(MustString(os.UserHomeDir()), ".config", "hub")
-	githubProvider   *provider.GithubProvider
 )
 
 func MustString(s string, err error) string {
@@ -46,68 +43,57 @@ func MustString(s string, err error) string {
 	return s
 }
 
-func githubCommand(command *kingpin.CmdClause, name string, help string) *kingpin.CmdClause {
-	c := command.Command(name, help)
-	c.Flag("github-token", "Github token. Can be read form hub config file").Envar("GITHUB_TOKEN").StringVar(&token)
-
-	return c
-}
-
-// github lazy load github provider
-func github() *provider.GithubProvider {
-	if githubProvider == nil {
-
-		if token == "" {
-			t, err := provider.ReadHubToken(defaultHubConfig)
-			if err != nil {
-				log.Fatalf("required flag '--%s'", "github-token")
-			}
-			token = t
-		}
-
-		r, err := provider.ParseRepo(*repo)
+func githubToken() string {
+	if *tokenFlag == "" {
+		t, err := provider.ReadHubToken(defaultHubConfig)
 		if err != nil {
-			log.Fatalf("Cannot parse repository")
+			log.Fatal("required flag '--github-token'")
 		}
-		githubRepo := r.(provider.GithubRepository)
-		githubProvider, _ = provider.NewGithubProvider(githubRepo.Owner, githubRepo.Repo, token, &provider.GithubProviderConfig{
-			Pattern: *pattern,
-			Branch:  *branch,
-		})
+		return t
+	} else {
+		return *tokenFlag
 	}
-
-	return githubProvider
-
 }
 
-func MustSetLoglevel(level string) {
+func mustSetLoglevel(level string) {
 	l, err := log.ParseLevel(level)
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
 	}
 	log.SetLevel(l)
 }
 
 func main() {
+	var err error
 	parse := kingpin.Parse()
 
-	MustSetLoglevel(*logLevel)
+	mustSetLoglevel(*logLevel)
 	log.WithFields(log.Fields{
 		"pattern": *pattern,
 	}).Debug(parse)
 
 	var f formatter.Formatter
 
+	pf := provider.ProviderFactory{
+		Pattern: *pattern,
+		Token:   githubToken(),
+	}
+
+	prov, err := pf.CreateProvider(*repo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	switch parse {
 	case "get next-version":
-		f = getNextVersion()
+		f = getNextVersion(prov)
 		// Releases
 	case "get releases":
-		f = getReleases()
+		f = getReleases(prov)
 	case "create release":
 		log.Warn("not implemented yet")
 	case "get changelog":
-		f = getChangelog()
+		f = getChangelog(prov)
 	}
 
 	if f != nil {
@@ -123,14 +109,14 @@ func main() {
 
 }
 
-func getNextVersion() formatter.Formatter {
-	r := github().GetNextRelease()
+func getNextVersion(prov provider.Provider) formatter.Formatter {
+	r := prov.GetNextRelease()
 	v, _ := r.NextVersion()
 	return &formatter.SimpleFormatter{Key: "next-version", Value: v}
 }
 
-func getReleases() formatter.Formatter {
-	r, err := github().GetReleases()
+func getReleases(prov provider.Provider) formatter.Formatter {
+	r, err := prov.GetReleases()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -138,15 +124,9 @@ func getReleases() formatter.Formatter {
 	return formatter.NewReleasesFormatter(m)
 }
 
-func getChangelog() formatter.Formatter {
-	var r *model.Release
-	if *release == "" {
-		r = github().GetNextRelease()
-	} else {
-		var err error
-		r, err = github().GetRelease(*release)
-		checkErr(err)
-	}
+func getChangelog(prov provider.Provider) formatter.Formatter {
+	r, err := prov.GetRelease(*release)
+	checkErr(err)
 	dto := formatter.MapRelease(r)
 	return formatter.NewChangelogFormatter(&dto, *color)
 }
